246fda97ae88b935b728d8de8170dadf65eca30c
LanceMontierro <lancemontierro21@gmail.com>
2025-04-13 13:20:39 +0800

    sds

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,11 @@ import "./globals.css";
 import { StatusBar } from "react-native";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-const publicKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
+  const publicKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
   return (
-<<<<<<< HEAD
     <ClerkProvider publishableKey={publicKey} tokenCache={tokenCache}>
-=======
-      <ClerkProvider publishableKey={publicKey} >
-          <ClerkLoaded>
->>>>>>> fd4e4f5483ffd8fcc424a56036293dbe03e5a441
       <>
         <StatusBar hidden={true} />
 
@@ -30,35 +25,12 @@ export default function RootLayout() {
             }}
           />
 
-<<<<<<< HEAD
           <Stack.Screen
-            name="home"
+            name="(home)"
             options={{
               headerShown: false,
             }}
           />
-=======
-            <Stack.Screen
-                name="(auth)/sign-in"
-                options={{
-                    headerShown: false,
-                }}
-            />
-
-            <Stack.Screen
-                name="(auth)/sign-up"
-                options={{
-                    headerShown: false,
-                }}
-            />
-
-            <Stack.Screen
-                name="(home)"
-                options={{
-                    headerShown: false,
-                }}
-            />
->>>>>>> fd4e4f5483ffd8fcc424a56036293dbe03e5a441
         </Stack>
       </>
     </ClerkProvider>
