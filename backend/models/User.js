9e1b962020bf6e8db2771ad7e292969becd7131b
LeoHunts <gobu1@email.com>
2025-04-15 15:42:10 +0800

    Add all schemas

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,6 +19,19 @@ const userSchema = mongoose.model(
         link: { type: String, required: true },
       },
     ],
+      recentlyViewed: [
+          {
+              title: { type: String, required: true },
+              description: { type: String, required: true },
+              poster: { type: String, required: true },
+              genre: { type: String, required: true },
+              releaseDate: { type: String, required: true },
+              rating: { type: String, required: true },
+              awards: { type: [String], required: true },
+              link: { type: String, required: true },
+              viewedAt: { type: Date, default: Date.now }
+          },
+      ],
   })
 );
 
