399894153a5b55f38aea5a98579c0a9351c90eb9
waitlangpar <germodenmhar@gmail.com>
2025-05-11 01:59:46 +0800

    itona

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,9 +16,11 @@ import React, { useState } from "react";
 import SampleTop from "@/components/SampleTop";
 import SampleLatest from "@/components/SampleLatest";
 import {images} from "@/constansts/images";
+import SearchBar2 from "@/components/SearchBar2";
+import {icons} from "@/constansts/icons";
 
 const IMAGES = [
-    require("../../assets/images/poster1.png"),
+    require("../../assets/images/po1.png"),
     require("../../assets/images/poster2.png"),
     require("../../assets/images/poster3.png"),
     require("../../assets/images/poster4.png"),
@@ -60,52 +62,45 @@ const Index = () => {
   return (
     <>
       <View className="flex-1 bg-[#282828]">
+
+          <Image
+              source={images.upperhome}
+              className="absolute w-full z-0"
+              resizeMode="cover"
+          />
+
         <ScrollView
           className="flex-1"
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{
             paddingBottom: 40,
             minHeight: isLandscape ? height + 550 : height + 450,
+              paddingHorizontal: 20,
           }}
         >
-            <Image
-                source={images.upperhome}
-                className="absolute w-full z-0"
-                resizeMode="cover"
-            />
 
+            <View className="justify-center items-center">
+                <Image
+                    source={icons.splashicon}
+                    className={`${
+                        isLandscape
+                            ? "w-12 h-10 mt-6 mx-auto"
+                            : "w-12 h-10 mt-16 mb-5 mx-auto"
+                    }`}
+                />
+            </View>
 
+            <View className={`${isLandscape ? 'mt-4' : 'mt-10'}`}>
+                <SearchBar2 placeholder={"Search MMFF Movies"} onPress={() => {
+                    router.push("/search");
+                }}/>
+            </View>
 
-            {/*<View className={`${isLandscape ? "mt-4" : "mt-10"}`}>*/}
-            {/*    <FlatList*/}
-            {/*        data={IMAGES}*/}
-            {/*        keyExtractor={(item, index) => index.toString()}*/}
-            {/*        style={{width: 500}}*/}
-            {/*        renderItem={({ item }) => (*/}
-            {/*            <Image*/}
-            {/*                source={item}*/}
-            {/*                style={{ width: 100, height: 150, marginRight: 10 }}*/}
-            {/*                resizeMode="cover"*/}
-            {/*            />*/}
-            {/*        )}*/}
-            {/*        horizontal*/}
-            {/*        showsHorizontalScrollIndicator={false}*/}
-            {/*    />*/}
-
-            {/*    <View className="flex-row justify-between items-center mt-4 px-8">*/}
-            {/*    <Text style={{textAlign: 'right' }} className="text-white mr-4 mt-4">*/}
-            {/*        NEW MMFFs*/}
-            {/*    </Text>*/}
-            {/*        <Text style={{textAlign: 'right' }} className="text-white mr-4 mt-4">*/}
-            {/*            OLD MMFFs*/}
-            {/*        </Text>*/}
-            {/*    </View>*/}
-            {/*</View>*/}
 
           <SampleLatest data={latestMovies} />
           <SampleTop data={topRatedMovies} />
-
           <SampleRecent data={recentOpenMovies} />
+
         </ScrollView>
       </View>
     </>
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -125,16 +125,21 @@ const Profile = () => {
   return (
 
       <View className="flex-1 bg-[#282828]">
-        <Image
-            source={images.bahay}
-            className={`${isLandscape ? 'absolute w-full z-0' : 'absolute w-full z-0'}`}
-            resizeMode="cover"
-        />
 
         <ScrollView
             showsVerticalScrollIndicator={false}
             contentContainerStyle={{ minHeight:  isLandscape ? height * 2.6 :"124%" }}
         >
+
+        <Image
+            source={images.bahay}
+            className={`${isLandscape ? 'absolute w-full z-0' : 'absolute w-full z-0'}`}
+            style={{borderRadius: 20}}
+            resizeMode="cover"
+        />
+
+
+
           <View className="justify-center items-center">
             <View className="bg-[#2E2E2E] mt-24 z-10 w-[317] h-[267] rounded-[25] justify-center items-center">
               <Image source={images.Star1} className="mt-[-50]" />
@@ -149,7 +154,7 @@ const Profile = () => {
               )}
             </View>
 
-            <View className="bg-[#787878] z-20 w-[345] h-[267] mt-[-55] rounded-[25] p-4">
+            <View className="bg-[#787878] z-20 w-[370] h-[267] mt-[-55] rounded-[25] p-4">
               <Text className="text-[#D9D9D9] text-lg mb-4">Account</Text>
 
               <TouchableOpacity
@@ -205,7 +210,7 @@ const Profile = () => {
 
             <View className="mt-4" />
 
-            <View className="bg-[#787878] z-20 w-[345] h-[267] rounded-[25] p-4">
+            <View className="bg-[#787878] z-20 w-[370] h-[267] rounded-[25] p-4">
               <Text className="text-[#D9D9D9] text-lg mb-4">General</Text>
 
               <TouchableOpacity
